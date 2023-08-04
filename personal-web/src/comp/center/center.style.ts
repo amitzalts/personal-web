c46206bffc6e4fe999b8a0ec6a6aa95715c8b591
amitzalts <amitzalts@gmail.com>
2023-08-04 12:46:34 +0300

    center comp init

--- personal-web/src/comp/center/center.style.ts
+++ personal-web/src/comp/center/center.style.ts
@@ -1,6 +1,40 @@
-import { TextField } from "@mui/material";
+// import { TextField } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export const Text = styled('div')({
-    backgroundColor : 'red'
+export const Profession = styled('div')({
+    color: 'white',
+    padding: '2rem',
+    fontWeight: '700',
+    fontSize:'3rem',
+})
+
+export const Name = styled('div')({
+    color: 'white',
+    padding: '2rem',
+    fontWeight: '700',
+    fontSize:'3rem',
+})
+
+export const PersonalDetailsWrapper = styled('div')({
+    width:'500px',
+    display:'flex',
+    flexDirection:'column',
+    backgroundColor: 'transparent',
+})
+
+export const PersonalImage = styled('div')({
+    width:'400px',
+    display:'flex',
+    flexDirection:'column',
+    backgroundImage:'url("https://www.freeiconspng.com/thumbs/penguin-png/madagascar-penguin-png-1.png")',
+    backgroundSize:'contain',
+    backgroundRepeat:'no-repeat',
+})
+
+export const Container = styled('div')({
+    width:'1515px',
+    height:'500px',
+    backgroundColor: 'darkslateblue',
+    display:'flex',
+    justifyContent:'space-around'
 })
--- personal-web/src/comp/center/center.tsx
+++ personal-web/src/comp/center/center.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import * as S from "./center.style"
 
- const TopCenter = () => {
-    // some funtion and use state
+const TopCenter = () => {
+  // some funtion and use state
   return (
     <>
-    <S.Text>Top Center</S.Text>
+      <S.Container>
+        <S.PersonalDetailsWrapper>
+          <S.Profession>Full-Stack Developer</S.Profession>
+          <S.Name>Tomer Cohen</S.Name>
+        </S.PersonalDetailsWrapper>
+        <S.PersonalImage></S.PersonalImage>
+      </S.Container>
     </>
   )
 }
