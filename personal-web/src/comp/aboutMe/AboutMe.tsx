73a92d6577e4309fef512efe6d77e1774324638c
tomer cohen <tomer66331@gmail.com>
2023-08-04 19:40:01 +0300

    scrolle working

--- personal-web/src/comp/aboutMe/AboutMe.tsx
+++ personal-web/src/comp/aboutMe/AboutMe.tsx
@@ -6,6 +6,7 @@ import * as S from "./AboutMe.style"
   return (
     <>
     <S.Text>About Me</S.Text>
+    <img src={"https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"} alt="Logo" />
     </>
   )
 }
--- personal-web/src/comp/navBar/NavBar.style.ts
+++ personal-web/src/comp/navBar/NavBar.style.ts
@@ -1,6 +1,37 @@
-import { TextField } from "@mui/material";
+// styles.js
 import { styled } from "@mui/material/styles";
 
-export const Text = styled('div')({
-    backgroundColor : 'red'
-})
+export const Nav = styled("ul")({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "center",
+  padding: 0,
+  listStyle: "none",
+  backgroundColor: "#f0f0f0",
+  borderRadius: "10px",
+  width: "100%",
+  margin: "0",
+  position: "fixed",
+  top: 0,
+
+  "@media (max-width: 768px)": {
+    flexDirection: "column",
+    width: "60px",
+  },
+});
+
+export const NavItem = styled("li")({
+  margin: "10px 0",
+  cursor: "pointer",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  height: "40px",
+  borderRadius: "5px",
+  transition: "background-color 0.2s ease",
+  "&:hover": {
+    backgroundColor: "#ddd",
+  },
+});
