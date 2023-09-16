075fdd94eee61f70c0b6bac3bb0db3fc9e797c6d
tomer cohen <tomer66331@gmail.com>
2023-09-17 00:45:54 +0300

    PDF WORK WELL

--- personal-web/src/comp/aboutMe/AboutMe.tsx
+++ personal-web/src/comp/aboutMe/AboutMe.tsx
@@ -1,34 +1,39 @@
 // AboutMe.js
 import React, { useRef } from 'react';
-import * as S from "./AboutMe.style";
+import * as S from './AboutMe.style';
+import pdfFile from '../../files/Tomer Cohen -09.2023 (1).pdf'; // Import your PDF file
 
 const AboutMe = () => {
   const projectsRef = useRef(null);
 
   const scrollToRef = (ref: any) => {
     ref.current.scrollIntoView({
-      behavior: "smooth",
+      behavior: 'smooth',
     });
-  };
-
-  const handleDownload = () => {
-    const pdfFilePath =  "../../files/Tomer-Cohen-resume.pdf" ; 
-    const anchor = document.createElement('a');
-    anchor.href = pdfFilePath;
-    anchor.download = 'Tomer-Cohen-resume.pdf';
-    anchor.click();
   };
 
   return (
     <S.Container>
       <S.ImageContainer>
-        <img src="https://www.freeiconspng.com/thumbs/penguin-png/madagascar-penguin-png-1.png" alt="Profile" />
+        <img
+          src="https://www.freeiconspng.com/thumbs/penguin-png/madagascar-penguin-png-1.png"
+          alt="Profile"
+        />
       </S.ImageContainer>
       <S.TextContainer>
         <S.Text>About Me</S.Text>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus consectetur incidunt enim illum possimus voluptatibus, dolorem quasi, exercitationem minus iste accusantium, impedit nihil aliquid ab fuga repudiandae at voluptate corporis.</p>
-        <S.DownloadButton onClick={handleDownload}>Download PDF</S.DownloadButton>
-        <S.DownloadButton onClick={() => scrollToRef(projectsRef)}>PROJECTS</S.DownloadButton>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          consectetur incidunt enim illum possimus voluptatibus, dolorem quasi,
+          exercitationem minus iste accusantium, impedit nihil aliquid ab fuga
+          repudiandae at voluptate corporis.
+        </p>
+        <a href={pdfFile} download="Tomer Cohen Resume">
+          <S.DownloadButton>Download PDF</S.DownloadButton>
+        </a>
+        <S.DownloadButton onClick={() => scrollToRef(projectsRef)}>
+          PROJECTS
+        </S.DownloadButton>
         <section ref={projectsRef} />
       </S.TextContainer>
     </S.Container>
